524af1efa57906d36f764a2ca2bde1953ecc6268
Yong Lim <yong.lim@gmail.com>
2023-01-12 13:07:32 +0700

    Codes to process contact form -> send data to Google Sheets

---
+++ assets/js/jordan.js
@@ -0,0 +1,64 @@
+$(function() {
+  $('.form-control').focus(formFocus);
+});
+
+function formFocus() {
+  $('#alert-field')
+    .removeClass()
+    .addClass('hidden');
+}
+
+function sendEmailFallLikeRain(e) {
+  e.preventDefault();
+
+  console.log("in jordan.js");
+  
+  // const POST_URL = 'https://script.google.com/macros/s/AKfycbwQ_wdqOBA_Z-29b9s2BZ7GHB3bGhlVzRL2hNwKdp-KoTQ5vhQ5bxz8uinBuKwvKDzP/exec'
+  // const postRequest = {
+  //   name: e.target['name-field'].value,
+  //   phone: e.target['phone-field'].value,
+  //   email: e.target['email-field'].value,
+  //   body: e.target['body-field'].value
+  // };
+  //
+  const POST_URL = 'https://script.google.com/macros/s/AKfycbxYtKqWsyoOoQFIxPq7BisC-2Xnlcrgka9-MAwQyWd-K1LZOjOsQWuQg951OUk4SyOk7w/exec'
+  const postRequest = {
+    name: e.target['name-field'].value,
+    email: e.target['email-field'].value,
+    body: e.target['body-field'].value,
+  };
+
+  if(POST_URL) {
+    $.post(POST_URL, JSON.stringify(postRequest))
+      .then(res => {
+        e.target.reset();
+        $('#alert-field')
+          .removeClass()
+          .addClass(`alert alert-${res.code}`)
+          .text(res.msg);
+      });
+
+    $('#alert-field')
+      .removeClass()
+      .html('<progress></progress>')
+      .removeClass('hidden');
+  } else {
+    alert('You must set the POST_URL variable with your script ID');
+  }
+}
+
+function changeSubject(e) {
+  if(e.target.value === 'Other') {
+    $('#subject-select').removeClass('col-xs-12')
+      .addClass('col-xs-6');
+    $('#hidden-other-subject').removeClass('hidden');
+  } else {
+    $('#subject-select').removeClass('col-xs-6')
+      .addClass('col-xs-12');
+
+    $('#hidden-other-subject').addClass('hidden');
+  }
+}
+
+
+
